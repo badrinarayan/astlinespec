17ec3782023e77b20d9b0601ef9543f396aac888
Badri Narayan Bhaskar <badrinarayan@gmail.com>
2013-05-21 09:46:46 -0500

    Remove perf from setup

--- setup.m
+++ setup.m
@@ -2,10 +2,10 @@
 javaaddpath java
 addpath redir
 addpath util
-addpath perf
 addpath backends
 builddocsearchdb('html')
+disp('Congratulations! You have now successfully installed Atomic Line Spectral Estimation Toolbox. ');
 disp('cd to redir directory and install CVX and SDPT3 if you need those backends.');
 disp('To get started, type: showdemo astdemo');
-disp('For a list of files');
+disp('For a general overview, type help astlinespec');
 
